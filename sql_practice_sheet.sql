38e5e037373f643de7e020eb7cef3f1285e4d574
MALAPAKA DATTA VENKATA NAGA SAI DEEPAK <154827923+DeepakMalapaka@users.noreply.github.com>
2024-08-24 17:00:24 +0530

    Update sql_practice_sheet.sql

--- sql_practice_sheet.sql
+++ sql_practice_sheet.sql
@@ -60,3 +60,71 @@ select ename,dname from emp,dept  where emp.deptno=dept.deptno and comm is not n
 -- 17) Display the empno, ename, sal, and salary increased by 15%
 select empno,ename,sal as salary,((sal*0.15)+sal) as updated_salary from emp;
 -- 18) Display employee names and corresponding manager names
+select e1.ename as employee,e2.ename as manager from emp e1,emp e2 where e1.mgr=e2.empno;
+-- 19) Display all the departments where employee salary greater than average salary of that department.
+select dname
+from dept
+where deptno in(select deptno 
+    			from emp
+    	  		group by deptno
+    			having max(sal)>avg(sal));
+-- 20)Display employees where length of ename is 5
+select ename 
+from emp
+where length(ename)=5;
+-- 21) Display all employees where ename start with J and ends with S
+select ename
+from emp
+where ename like 'j%s' or ename like 'J%S';
+-- 22) Display all employees where employee does not belong to 10,20,40
+select ename
+from emp
+where deptno not in(10,20,40);
+-- 23) Display all employees where jobs does not belong to PRESIDENT and MANAGER.
+select ename
+from emp
+where job not in('president','manager');
+-- 24) Display the maximum salary in the emp table
+select max(sal)
+from emp;
+-- 25) Display average salary for job SALESMAN
+select avg(sal)
+from emp
+where job='SALESMAN';
+-- 26) Display all ename where first character could be anything, but second character should be L.
+select ename
+from emp
+where ename like '_L%';
+-- 27) Display nth highest and nth lowest salary in emp table.
+
+-- 28) Display all the departments where department has 3 employees.
+select dname
+from dept
+where deptno in (select deptno
+    			from emp
+    			group by deptno
+    			having count(*)=3);
+-- 29) Display sum of salary for each department. 
+select sum(sal)
+from emp
+group by deptno;
+-- 30) Display all department with Minimum salary and maximum salary?
+select max(sal),min(sal)
+from emp
+group by deptno;
+-- 31) Display all ename, empno, dname, loc from emp, dept table.
+select ename, empno, dname, loc 
+from emp natural join dept;
+-- 32) Display all the departments where department does not have any employees
+select dname
+from dept 
+where deptno not in(select distinct deptno
+    				from emp);
+-- 33) Display all the departments where department does have atleast one employee
+select dname 
+from dept
+where deptno in (select distinct deptno
+    			from emp
+    			group by deptno
+    			having count(*)>=1);
+-- 34) Display all employees those who are not managers.
